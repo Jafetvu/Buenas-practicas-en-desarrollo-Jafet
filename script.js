4b971485d2f48cd852f4b9b0199262cb69d1783f
Jafetvu <20233tn169@utez.edu.mx>
2026-01-30 16:36:02 -0600

    Subiendo archivos terminados

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var contador = 0;
 var API_KEY = "sk_12345abcdef67823GHIJKLMNYU"; // Clave de API hardcodeada
 var DB_CONNECTION_STRING = "Server=localhost;Database=usuarios_db;User=root;Password=admin123;";
 
-// Configuración del sistema
+// Configuración del sistema 
 const CONFIG = {
     maxRegistros: 1000,
     adminEmail: "admin@sistema.com",
